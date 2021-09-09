bf835dccbc80938116331bfae33f205f2a585170
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-09-09 17:25:56 +0300

    Snake now can move with tail but just straight. WITHOUT MOUSE(FINGER)

--- Assets/_Scripts/Snake.cs
+++ Assets/_Scripts/Snake.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class Snake : MonoBehaviour
 {
     [SerializeField] private SnakeHead _snakeHead;
+    [Tooltip("The more speed of snake you add the bigger attraction you need.")]
+    [SerializeField] private float _speed;
+    [Tooltip("For example: great case is when snake`s speed equals 3 then attraction will be 15.")]
+    [SerializeField] private float _tailSegmentsAttraction;
 
     private List<Segment> _tail;
     private TailGenerator _tailGenerator;
@@ -14,6 +18,31 @@ public class Snake : MonoBehaviour
     private void Awake()
     {
         _tailGenerator = GetComponent<TailGenerator>();
+
         _tail = _tailGenerator.Generate();
+    }
+
+    /// <summary>
+    /// better when need to continiously movement
+    /// always has same speed so the object moves without shakes but really smooth
+    /// </summary>
+    private void FixedUpdate()   
+    {
+        Move(_snakeHead.transform.position + _snakeHead.transform.up * _speed * Time.fixedDeltaTime);
+    }
+
+    private void Move(Vector3 nextPosition)
+    {
+        Vector3 previousPosition = _snakeHead.transform.position;
+
+        foreach (var segment in _tail)
+        {
+            Vector3 tempPosition = segment.transform.position;
+            segment.transform.position = Vector2.Lerp(segment.transform.position, previousPosition,
+                                                      _tailSegmentsAttraction * Time.deltaTime);
+            previousPosition = tempPosition;
+        }
+
+        _snakeHead.Move(previousPosition);
     }
 }
